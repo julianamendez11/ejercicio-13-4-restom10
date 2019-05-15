7f9888e55cb0ba72b4691bc7b2c2fde8c034faf7
Default user <v.martin@uniandes.edu.co>
2019-05-15 23:54:43 +0000

    archivo

--- plot.py
+++ plot.py
@@ -1,20 +1,34 @@
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
-from matplotlib import style
-from matplotlib import cm
 import numpy as np
+import matplotlib.pylab as plt
+from scipy import interpolate
+datos=np.genfromtxt("datos.dat", delimiter=",")
+datos1=np.genfromtxt("datos1.dat", delimiter=",")
+x=datos[:,0]
+y=datos[:,1]
+x1=datos1[:,0]
+y1=datos1[:,1]
+plt.figure()
+plt.plot(x1,y1, c="fuchsia",label='theta =0.3')
+plt.plot(x,y, c="red",label='theta =0.1')
+plt.xlabel("tiempo(s)")
+plt.ylabel("Angulo theta")
+plt.title("primer intento")
+plt.savefig("figura.pdf")
+plt.legend()
+plt.show()
+plt.close()
 
-data = np.loadtxt("onda.dat")
 
-fig=plt.figure(figsize=(15,5))
-ax1 = fig.add_subplot(111,projection='3d')
-x, y=np.mgrid[0:data.shape[0], 0:data.shape[1]]
-ax1.plot_surface(x/100, y/100, data, cmap=cm.rainbow)
-plt.xlabel("Tiempo (segundos)")
-plt.ylabel("Posición (metros)")
-
-
-
+plt.figure()
+plt.plot(x, y2, 'o')
+plt.plot(x,p(x),c='red')
+plt.plot(x,p(x),c='red',label='regresion')
+plt.title('Delta de angulos en regimen no caotico')
+plt.xlabel('Tiempo')
+plt.ylabel('Log del delta de theta')
+plt.savefig('DeltaAngulosNoCaotico.pdf')
 plt.show()
 plt.legend()
-plt.savefig("plot.png")
+plt.close()
+
+print("El coeficiente de Lyapunov: "+str(p[1])) 
