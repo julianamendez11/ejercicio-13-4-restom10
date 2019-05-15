6d47f5237aae955a036a40ce69ec395d8ba47e06
Restom <jr.restom10@uniandes.edu.co>
2019-05-15 20:53:23 +0000

    Diferencia de angulos con logaritmo

--- plot.py
+++ plot.py
@@ -2,16 +2,26 @@ import numpy as np
 import matplotlib.pylab as plt
 datos=np.genfromtxt("datos.dat", delimiter=",")
 datos1=np.genfromtxt("datos1.dat", delimiter=",")
+
 x=datos[:,0]
 y=datos[:,1]
 x1=datos1[:,0]
 y1=datos1[:,1]
 plt.figure()
 plt.plot(x1,y1, c="fuchsia",label='theta =0.3')
-plt.plot(x,y, c="red",label='theta =0.1')
+plt.plot(x,y, c="red",label='theta =0')
 plt.xlabel("tiempo(s)")
 plt.ylabel("Angulo theta")
 plt.title("primer intento")
 plt.savefig("figura.pdf")
 plt.legend()
+plt.close()
+
+y2=np.log(np.abs(y-y1))
+plt.figure()
+plt.scatter(x,y2,c='coral')
+plt.title('Delta de angulos')
+plt.xlabel('Tiempo')
+plt.ylabel('Log del delta de theta')
+plt.savefig('DeltaAngulos.pdf')
 plt.close()
