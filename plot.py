0187fb7aa1dc5cf334dcc35508db1f3ff7d93f60
Restom <jr.restom10@uniandes.edu.co>
2019-05-15 20:33:56 +0000

    Dos graficas

--- plot.py
+++ plot.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pylab as plt
 datos=np.genfromtxt("datos.dat", delimiter=",")
+datos1=np.genfromtxt("datos1.dat", delimiter=",")
 x=datos[:,0]
 y=datos[:,1]
+x1=datos1[:,0]
+y1=datos1[:,1]
 plt.figure()
-plt.plot(x,y, c="fuchsia")
+plt.plot(x1,y1, c="fuchsia",label='theta =0.3')
+plt.plot(x,y, c="red",label='theta =0.1')
 plt.xlabel("tiempo(s)")
 plt.ylabel("Angulo theta")
 plt.title("primer intento")
 plt.savefig("figura.pdf")
+plt.legend()
+plt.close()
